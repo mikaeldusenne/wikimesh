01ddb7a0120007cc97f9418f37662efc02d31cc4
Mikaël Dusenne <mikaeldusenne@gmail.com>
2021-09-10 18:24:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Creating a project
+
+## First run
+
+edit the desired node dependencies in frontend/init.sh
+
+run:
+
+```
+# initial setup on first run
+./run.sh init
+
+# only for building the frontend when you change it
+./run.sh build
+
+# run the project in production mode
+./run.sh prod
+```
+
+for a development environment:
+
+```
+./run.sh dev
+```
---
+++ run.sh
@@ -0,0 +1,64 @@
+
+#!/bin/sh
+
+CMD="$1"
+shift
+
+case "$CMD" in
+    ""|prod*)
+        docker-compose -f docker-compose.yml -f production.yml up --abort-on-container-exit $@
+        ;;
+    dev)
+        docker-compose -f docker-compose.yml  -f development.yml -f front-end.yml up --abort-on-container-exit $@
+        ;;
+    init)
+        echo "setting secrets..."
+        ./init_secrets.sh
+        echo "initializing frontend dependencies..."
+        docker-compose -f front-end.yml -f init_frontend.yml up --build --abort-on-container-exit $@
+        ;;
+    build*)
+        docker-compose -f front-end.yml -f build-front-end.yml up --abort-on-container-exit $@
+        ;;
+    lint*)
+        docker exec -it wikimesh_vuecli_1 npm run lint
+        ;;
+    dump-db)
+        docker exec -it wikimesh_mongo_docker sh -c "mongodump --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --db thedb --out /dumps/dump_$(date +'%Y-%m-%d_%H-%M-%S')"
+        ;;
+    restore-db)
+        docker exec -i wikimesh_mongo_docker sh -c "mongorestore --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD /dumps/$1"
+        ;;
+    connect-db)
+        . ./mongo/.env
+        case "$1" in
+            user)
+                user=$MONGO_INITDB_USER_USERNAME
+                pass=$MONGO_INITDB_USER_PASSWORD
+            ;;
+            root)
+                user=$MONGO_INITDB_ROOT_USERNAME
+                pass=$MONGO_INITDB_ROOT_PASSWORD
+            ;;
+        esac
+        docker exec -it wikimesh_mongo_docker mongo --username $user --password $pass
+        ;;
+    dev-venv-create)
+        python -m venv venv && . ./venv/bin/activate \
+            && pip install -r backend/requirements.txt \
+            ;;
+    init)
+        docker-compose -f docker-compose.yml -f front-end.yml build --no-cache && \
+            ./run.sh update && \
+            ./run.sh build --build && \
+            ./run.sh 
+        ;;
+    stop)
+        docker-compose down
+        ;;
+    *)
+        echo "unknown argument"
+        exit 1
+        ;;
+esac
+
