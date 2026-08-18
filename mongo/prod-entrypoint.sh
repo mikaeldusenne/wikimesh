e1524d784399d9bd35d7e7cff0caf7745406745c
Mikaël Dusenne <mikaeldusenne@gmail.com>
2026-08-18 05:07:43 -0400

    Remove custom Mongo entrypoint

--- mongo/prod-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-if [[ "$(id -u)" == 0 && -f /data/db/WiredTiger.wt ]]; then
-    exec gosu "$(stat -c '%u:%g' /data/db/WiredTiger.wt)" \
-        /usr/local/bin/docker-entrypoint.sh "$@"
-fi
-
-exec /usr/local/bin/docker-entrypoint.sh "$@"
